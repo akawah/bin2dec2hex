8005622fab0186bf5812ea1e33a4f6c926898987
V.Aysin aka Wah <icnu@ya.ru>
2019-09-24 02:52:29 +0300

    pwa 002

--- source/sw.js
+++ source/sw.js
@@ -2,17 +2,17 @@ self.addEventListener('install', function(event) {
   var indexPage = new Request('index.html');
   event.waitUntil(
     fetch(indexPage).then(function(response) {
-      return caches.open('pwabuilder-offline').then(function(cache) {
-        console.log('[PWA Builder] Cached index page during Install'+ response.url);
+      return caches.open('bin-dec-hex-offline').then(function(cache) {
+        console.log('bin-dec-hex Cached index page during Install'+ response.url);
         return cache.put(indexPage, response);
       });
   }));
 });
 self.addEventListener('fetch', function(event) {
   var updateCache = function(request){
-    return caches.open('pwabuilder-offline').then(function (cache) {
+    return caches.open('bin-dec-hex-offline').then(function (cache) {
       return fetch(request).then(function (response) {
-        console.log('[PWA Builder] add page to offline'+response.url)
+        console.log('bin-dec-hex add page to offline'+response.url)
         return cache.put(request, response);
       });
     });
@@ -20,8 +20,8 @@ self.addEventListener('fetch', function(event) {
   event.waitUntil(updateCache(event.request));
   event.respondWith(
     fetch(event.request).catch(function(error) {
-      console.log( '[PWA Builder] Network request Failed. Serving content from cache: ' + error );
-      return caches.open('pwabuilder-offline').then(function (cache) {
+      console.log( 'Network request Failed. Serving content from cache: ' + error );
+      return caches.open('bin-dec-hex-offline').then(function (cache) {
         return cache.match(event.request).then(function (matching) {
           var report =  !matching || matching.status == 404?Promise.reject('no-match'): matching;
           return report
