ef2874136b1c30bb1229b601edf569911fd1cbb2
V.Aysin aka Wah <icnu@ya.ru>
2019-12-07 18:25:43 +0300

    with babel

--- wo_babel/js/script.js
+++ wo_babel/js/script.js
@@ -1,22 +1,19 @@
-"use strict";
+let wrap = document.querySelector(".wrap");
+let calc = wrap.querySelector(".calc");
+let bin = calc.querySelector(".bin");
+let dec = calc.querySelector(".dec");
+let hex = calc.querySelector(".hex");
+let keysWrap = wrap.querySelector(".keys");
+let keys = keysWrap.querySelectorAll(".key");
 
-var wrap = document.querySelector(".wrap");
-var calc = wrap.querySelector(".calc");
-var bin = calc.querySelector(".bin");
-var dec = calc.querySelector(".dec");
-var hex = calc.querySelector(".hex");
-var keysWrap = wrap.querySelector(".keys");
-var keys = keysWrap.querySelectorAll(".key");
-
-var inputIncrement = function inputIncrement() {
+let inputIncrement = () => {
   dec.value++;
   bin.value = parseInt(dec.value, 10).toString(2);
   hex.value = parseInt(dec.value, 10).toString(16);
 };
 
-var inputDecrement = function inputDecrement() {
+let inputDecrement = () => {
   dec.value--;
-
   if (dec.value <= 0) {
     bin.value = dec.value = hex.value = "";
   } else {
@@ -25,7 +22,7 @@ var inputDecrement = function inputDecrement() {
   }
 };
 
-var updateFields = function updateFields(input) {
+let updateFields = input => {
   if (input.value == "") {
     bin.value = dec.value = hex.value = "";
   } else if (input.value == 0) {
@@ -44,13 +41,13 @@ var updateFields = function updateFields(input) {
   }
 };
 
-var getStringChunks = function getStringChunks(input) {
-  var start = input.selectionStart;
-  var end = input.selectionEnd;
-  var array = input.value.split("");
-  var leftArr = array.splice(0, start);
-  var selectionArr = array.splice(0, end - start);
-  var rightArr = array.splice(0);
+let getStringChunks = input => {
+  let start = input.selectionStart;
+  let end = input.selectionEnd;
+  let array = input.value.split("");
+  let leftArr = array.splice(0, start);
+  let selectionArr = array.splice(0, end - start);
+  let rightArr = array.splice(0);
   return {
     left: leftArr.join(""),
     selection: selectionArr.join(""),
@@ -58,41 +55,36 @@ var getStringChunks = function getStringChunks(input) {
   };
 };
 
-var inputSymbol = function inputSymbol(input, chunks, symbol) {
+let inputSymbol = (input, chunks, symbol) => {
   input.value = chunks.left + symbol + chunks.right;
   input.selectionStart = input.selectionEnd = chunks.left.length + 1;
   updateFields(input);
 };
 
 if (touch) {
-  window.onorientationchange = function() {
-    return document.activeElement.focus();
-  };
+  window.onorientationchange = () => document.activeElement.focus();
 
-  var inputBackspace = function inputBackspace(input, chunks) {
+  let inputBackspace = (input, chunks) => {
     if (!chunks.selection) {
       chunks.left = chunks.left.slice(0, -1);
     }
-
     input.value = chunks.left + chunks.right;
     input.selectionStart = input.selectionEnd = chunks.left.length;
     updateFields(input);
   };
 
-  var inputDelete = function inputDelete(input, chunks) {
+  let inputDelete = (input, chunks) => {
     if (!chunks.selection) {
       chunks.right = chunks.right.slice(1);
     }
-
     input.value = chunks.left + chunks.right;
     input.selectionStart = input.selectionEnd = chunks.left.length;
     updateFields(input);
   };
 
-  var inputLeft = function inputLeft(input, chunks) {
+  let inputLeft = (input, chunks) => {
     if (chunks.left.length > 0) {
       input.value = chunks.left + chunks.selection + chunks.right;
-
       if (!chunks.selection) {
         input.selectionStart = input.selectionEnd = chunks.left.length - 1;
       } else {
@@ -105,10 +97,9 @@ if (touch) {
     }
   };
 
-  var inputRight = function inputRight(input, chunks) {
+  let inputRight = (input, chunks) => {
     if (chunks.right.length > 0) {
       input.value = chunks.left + chunks.selection + chunks.right;
-
       if (!chunks.selection) {
         input.selectionStart = input.selectionEnd = chunks.left.length + 1;
       } else {
@@ -123,16 +114,16 @@ if (touch) {
     }
   };
 
-  document.addEventListener("mousedown", function(e) {
+  document.addEventListener("mousedown", e => {
     e.stopPropagation();
-    var target = e.target;
+    let target = e.target;
 
     if (!target.classList.contains("input")) {
       e.preventDefault();
     }
 
-    var input = document.activeElement;
-    var chunks = getStringChunks(input);
+    let input = document.activeElement;
+    let chunks = getStringChunks(input);
 
     if (target.classList.contains("key")) {
       inputSymbol(input, chunks, target.textContent);
@@ -150,31 +141,32 @@ if (touch) {
       inputIncrement(input);
     }
   });
-  calc.addEventListener("click", function(e) {
-    var target = e.target;
 
+  calc.addEventListener("click", e => {
+    let target = e.target;
     if (target == bin) {
-      for (var i = 0, len = keys.length; i < len; i++) {
+      for (let i = 0, len = keys.length; i < len; i++) {
         if (keys[i].classList.contains("key-bin")) keys[i].disabled = false;
         else keys[i].disabled = true;
       }
     } else if (target == dec) {
-      for (var _i = 0, _len = keys.length; _i < _len; _i++) {
-        if (keys[_i].classList.contains("key-hex")) keys[_i].disabled = true;
-        else keys[_i].disabled = false;
+      for (let i = 0, len = keys.length; i < len; i++) {
+        if (keys[i].classList.contains("key-hex")) keys[i].disabled = true;
+        else keys[i].disabled = false;
       }
     } else if (target == hex) {
-      for (var _i2 = 0, _len2 = keys.length; _i2 < _len2; _i2++) {
-        keys[_i2].disabled = false;
+      for (let i = 0, len = keys.length; i < len; i++) {
+        keys[i].disabled = false;
       }
     }
   });
 } else {
   // desktop
-  var binInclude = ["0", "1"];
-  var decInclude = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-  var allowLetters = ["KeyA", "KeyB", "KeyC", "KeyD", "KeyE", "KeyF"];
-  var allowKeys = [
+
+  let binInclude = ["0", "1"];
+  let decInclude = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  let allowLetters = ["KeyA", "KeyB", "KeyC", "KeyD", "KeyE", "KeyF"];
+  let allowKeys = [
     "Tab",
     "ShiftLeft",
     "ShiftRight",
@@ -188,19 +180,17 @@ if (touch) {
     "Backspace"
   ];
 
-  var contains = function contains(arr, key) {
-    var len = arr.length;
-
+  let contains = (arr, key) => {
+    let len = arr.length;
     for (var i = 0; i < len; i++) {
       if (arr[i] === key) {
         return true;
       }
     }
-
     return false;
   };
 
-  bin.onkeydown = function(e) {
+  bin.onkeydown = e => {
     if (contains(binInclude, e.key) || contains(allowKeys, e.code)) {
       return true;
     } else if (e.code == "ArrowUp") {
@@ -208,15 +198,13 @@ if (touch) {
     } else if (e.code == "ArrowDown") {
       inputDecrement();
     }
-
     return false;
   };
-
-  bin.onkeyup = function() {
+  bin.onkeyup = () => {
     updateFields(bin);
   };
 
-  dec.onkeydown = function(e) {
+  dec.onkeydown = e => {
     if (contains(decInclude, e.key) || contains(allowKeys, e.code)) {
       return true;
     } else if (e.code == "ArrowUp") {
@@ -224,39 +212,35 @@ if (touch) {
     } else if (e.code == "ArrowDown") {
       inputDecrement();
     }
-
     return false;
   };
-
-  dec.onkeyup = function() {
+  dec.onkeyup = () => {
     updateFields(dec);
   };
 
-  hex.onkeydown = function(e) {
+  hex.onkeydown = e => {
     if (contains(decInclude, e.key) || contains(allowKeys, e.code)) {
       return true;
     } else if (contains(allowLetters, e.code)) {
-      var chunks = getStringChunks(hex);
+      let chunks = getStringChunks(hex);
       inputSymbol(hex, chunks, e.code.toString().slice(-1));
     } else if (e.code == "ArrowUp") {
       inputIncrement();
     } else if (e.code == "ArrowDown") {
       inputDecrement();
     }
-
     return false;
   };
-
-  hex.onkeyup = function() {
+  hex.onkeyup = () => {
     updateFields(hex);
   };
 
-  document.querySelector(".top").onclick = function(e) {
+  document.querySelector(".top").onclick = e => {
     e.stopPropagation();
     inputIncrement();
   };
 
-  document.querySelector(".bottom").onclick = function(e) {
+  document.querySelector(".bottom").onclick = e => {
     e.stopPropagation();
     inputDecrement();
   };
